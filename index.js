6d9fa2ad042366b12397c88afe9fd9a9e7391620
John Fawcett <jrf0110@gmail.com>
2013-03-31 14:18:46 -0500

    Update index.js

--- index.js
+++ index.js
@@ -8,10 +8,20 @@ define(function(require){
   exports.ajax = exports.http = req;
 
   exports.stage = function(fns){
-    var call = function(name){
-      fns[name].apply(null, Array.prototype.slice.call(arguments, 1).concat(call));
+    var current = function(){
+      var args = Array.prototype.slice.call(arguments, 0);
+      var callback = args.pop();
+  
+      // Redefine current after first call so that it doesn't default to 'start'
+      current = function(name){
+        if (!fns.hasOwnProperty(name)) throw new Error('Cannot find stage item: ', name);
+        fns[name].apply(null, Array.prototype.slice.call(arguments, 1).concat(current, callback));
+      };
+  
+      fns.start.apply(null, args.concat(current, callback));
     };
-    return call('start');
+  
+    return current;
   };
 
 
